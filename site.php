d9cd089bf546b073bfa374d4a230015ba3349407
SamuelHenrique32 <shdalmas@ucs.br>
2018-10-14 19:00:18 -0300

    Site Cadastro OK

--- site.php
+++ site.php
@@ -122,7 +122,9 @@ $app->get("/checkout", function(){
 $app->get("/login", function(){                                 //mostra pagina de login
     $page = new Page();
     $page->setTpl("login", [
-        'error'=>User::getError()
+        'error'=>User::getError(),
+        'errorRegister'=>User::getErrorRegister(),
+        'registerValues'=>(isset($_SESSION['registerValues'])) ? $_SESSION['registerValues'] : ['name'=>'', 'email'=>'', 'phone'=>'']
     ]);
 });
 
@@ -139,6 +141,53 @@ $app->post("/login", function(){                                //recupera via p
 $app->get("/logout", function(){
     User::logout();
     header("Location: /login");
+    exit;
+});
+
+$app->post("/register", function(){
+    $_SESSION['registerValues'] = $_POST;                                                //nao limpar dados no formulario de login
+    //$_SESSION['postValues']['password'] = '';
+    if (!isset($_POST['name']) || $_POST['name']=='') {                                  //nao definido ou vazio
+        User::setErrorRegister("Preencha o seu nome.");
+        header('Location: /login');
+        exit;
+    }
+    if (!isset($_POST['email']) || $_POST['email']=='') {
+        User::setErrorRegister("Preencha o seu e-mail.");
+        header('Location: /login');
+        exit;
+    }
+    if (!isset($_POST['password']) || $_POST['password']=='') {
+        User::setErrorRegister("Preencha a senha.");
+        header('Location: /login');
+        exit;
+    }
+    if (User::checkLoginExist($_POST['email'])) {
+        User::setErrorRegister("Este usuário já está cadastrado. Use a opção esqueci a senha.");
+        header('Location: /login');
+        exit;
+    }
+    $user = new User();
+    $user->setData([
+        'inadmin'=>0,                                                               //nao e adm
+        'deslogin'=>$_POST['email'],
+        'desperson'=>$_POST['name'],
+        'desemail'=>$_POST['email'],
+        'despassword'=>$_POST['password'],                                          //metodo save faz criptografia
+        'nrphone'=>$_POST['phone']
+    ]);
+
+    $user->save();
+    User::login($_POST["email"], $_POST["password"]);
+    /*try {
+        User::login($_POST["email"], $_POST["password"]);
+    } catch (Exception $e) {
+        User::setError($e->getMessage());
+        header('Location: /login');
+        exit;
+    }
+    $_SESSION['postValues'] = ['name'=>'', 'email'=>'', 'phone'=>''];*/
+    header('Location: /checkout');
     exit;
 });
 
