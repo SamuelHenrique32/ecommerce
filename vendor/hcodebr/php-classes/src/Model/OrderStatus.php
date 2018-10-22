40d1c67b30819b39f5b892d3af49e69776117954
SamuelHenrique32 <shdalmas@ucs.br>
2018-10-21 22:10:50 -0300

    Admin - Pedidos OK

--- vendor/hcodebr/php-classes/src/Model/OrderStatus.php
+++ vendor/hcodebr/php-classes/src/Model/OrderStatus.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Hcode\Model;
-
 use \Hcode\DB\Sql;
 use \Hcode\Model;
 
@@ -10,6 +9,10 @@ class OrderStatus extends Model {
     const AGUARDANDO_PAGAMENTO = 2;
     const PAGO = 3;
     const ENTREGUE = 4;
+    public static function listAll()
+    {
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_ordersstatus ORDER BY desstatus");
+    }
 }
-
 ?>
